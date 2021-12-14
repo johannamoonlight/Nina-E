7c9a7689b712997424a0c6b721057fa5a60b68c2
johanna gonzalez <jowie666@outlook.com>
2021-12-14 17:02:53 -0300

    cartview

--- src/componentes/CartView.js
+++ src/componentes/CartView.js
@@ -14,7 +14,7 @@ export const CartView = () => {
         <h1>{i.item.product1}</h1>
         <h2>Precio indivudual : ${i.item.precio}</h2>
         <h2>Cantidad: {i.cantidad}</h2>
-        <h2>SubTotal:{i.cantidad = i.item.precio} </h2>
+        <h2>SubTotal:{i.cantidad * i.item.precio} </h2>
         <img src ={i.item.imagen} alt = {i.item.product1} style ={{height: "50px", width:"50px"}}/>
         <button>Eliminar producto</button>
     
